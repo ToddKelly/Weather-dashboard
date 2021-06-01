0c7816f822e687015a2557681effede312236d40
Todd Kelly <donkeyskates21@gmail.com>
2021-06-01 15:13:31 -0400

    added index, css, js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need to enter a city to search for.');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
