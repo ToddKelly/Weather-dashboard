8abd76e7e6ecf182f2ae0a6c519cc6d8ac4e55e7
Todd Kelly <donkeyskates21@gmail.com>
2021-06-27 22:17:06 -0400

    Added Api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,13 @@ function handleSearchFormSubmit(event) {
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+var fiveDayF = "https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=3b9b4c043ed6400637bf4bad42b80e7b"
+var curDayF = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
+
+//Time settings
+$("#time").text(moment().format("dd, MM Do YYYY, hh:mm:ss"))
+setInterval(function(){
+    $("#time").text(moment().format("dddd, MMMM Do YYYY, hh:mm:ss"))
+},1000)
